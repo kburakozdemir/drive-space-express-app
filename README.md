94cf439f83f686332a43d996bc212cdf19155b7c
K. Burak Ozdemir <kburakozdemir@gmail.com>
2022-12-24 16:48:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Notes
+
+```bash
+# Node v16.13.2
+npm install
+```
+
+```bash
+npm run start # http://localhost:4000
+```
+
+## References
+
+https://dev.to/cassiolacerda/automatically-refresh-the-browser-on-node-express-server-changes-x1f680-1k0o
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+var fetch = require("node-fetch");
+
+require("dotenv").config({ path: __dirname + "/../.env" });
+
+const HASURA_GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET;
+
+let url = process.env.HASURA_GRAPHQL_URL
+
+let settings = {
+  method: "Get",
+  headers: {
+    "Content-Type": "application/json",
+    "X-Hasura-Admin-Secret": HASURA_GRAPHQL_ADMIN_SECRET,
+  },
+};
+
+let driveSpaceData = "";
+
+fetch(url, settings)
+  .then((res) => res.json())
+  .then((json) => {
+    // do something with JSON
+    driveSpaceData = json.server_space_latest_data;
+  });
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Drive Space", driveSpaceData });
+});
+
+module.exports = router;
